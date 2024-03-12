5414c2026617cda26aab124e05696b0637d7fa91
Edwin Tijaro <e.tijaro@uniandes.edu.co>
2024-03-11 21:17:32 -0500

    pantallas de puzzles creadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,18 @@ import { LoginComponent } from './users/login/login.component';
 import { SignUpComponent } from './users/sign-up/sign-up.component';
 import { RecoveryComponent } from './users/recovery/recovery.component';
 import { HomeComponent } from './app-home/home/home.component';
+import { PuzzlesComponent } from './crear-puzzles/puzzles/puzzles.component';
+import { PuzzleComponent } from './puzzle/puzzle.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirección de la ruta vacía a /home
   { path: 'login', component: LoginComponent,  pathMatch: 'full' },
   { path: 'signup', component: SignUpComponent,  pathMatch: 'full' },
   { path: 'recovery', component: RecoveryComponent,  pathMatch: 'full' },
-  { path: 'home', component: HomeComponent,  pathMatch: 'full' }
+  { path: 'home', component: HomeComponent,  pathMatch: 'full' },
+  { path: 'puzzle', component: PuzzlesComponent,  pathMatch: 'full' },
+  { path: 'crearpuzzle', component: PuzzleComponent,  pathMatch: 'full' }
+
 ];
 
 @NgModule({
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-import { RecoveryComponent } from './recovery/recovery.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [LoginComponent, SignUpComponent, RecoveryComponent],
-  exports: [LoginComponent, SignUpComponent, RecoveryComponent]
+  declarations: [LoginComponent, SignUpComponent ],
+  exports: [LoginComponent, SignUpComponent ]
 })
 export class UsersModule { }
