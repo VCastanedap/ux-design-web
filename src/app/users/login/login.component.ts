71b7e8548bad01bb07fff4703f03234406c91848
Victor Castaneda <v.castanedap@uniandes.edu.co>
2024-03-12 16:57:05 -0500

    Login

--- src/app/users/login/login.component.ts
+++ src/app/users/login/login.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+import {
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  passwordFormControl = new FormControl('', [
+      Validators.required, 
+      Validators.pattern(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&^_-]).{8,}/)
+    ]
+  );
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  matcher = new MyErrorStateMatcher();
 
 }
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -4,11 +4,28 @@ import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { RecoveryComponent } from './recovery/recovery.component';
 
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { ErrorStateMatcher, ShowOnDirtyErrorStateMatcher } from '@angular/material/core';
+
+import {MatCardModule} from '@angular/material/card'; 
+import {MatButtonModule} from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+
 @NgModule({
+  providers: [
+    {provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher}
+  ],
   imports: [
-    CommonModule
+    CommonModule, MatCardModule, MatButtonModule, MatGridListModule, MatIconModule, FormsModule, MatFormFieldModule,
+    MatInputModule, ReactiveFormsModule
   ],
   declarations: [LoginComponent, SignUpComponent, RecoveryComponent],
   exports: [LoginComponent, SignUpComponent, RecoveryComponent]
 })
 export class UsersModule { }
+
