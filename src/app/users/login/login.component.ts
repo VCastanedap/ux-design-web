831405490096c4897179c68224c239bb23a7c54e
Victor Castaneda <v.castanedap@uniandes.edu.co>
2024-03-12 17:09:57 -0500

    Redirects

--- src/app/users/login/login.component.ts
+++ src/app/users/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+
 import {
   FormControl,
   FormGroupDirective,
@@ -25,6 +27,22 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  constructor(private router: Router) { }
+  
+  redirectToHome(): void {
+    this.router.navigate(['/home']);
+  }
+
+  redirectToSignUp(): void {
+    this.router.navigate(['/signup']);
+  }
+
+  redirectToRecovery(): void {
+    this.router.navigate(['/recovery']);
+  }
+
+
   passwordFormControl = new FormControl('', [
       Validators.required, 
       Validators.pattern(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&^_-]).{8,}/)
